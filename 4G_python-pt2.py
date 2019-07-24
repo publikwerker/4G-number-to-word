b87eb52b95812770d4d0e11c9e80a9ab74b2dd20
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-24 14:37:58 -0700

    created word bank and basic word to number function for individual items in list

--- 4G_python-pt2.py
+++ 4G_python-pt2.py
@@ -13,12 +13,47 @@
 # Input: Ten Thousand Four Hundred Eighty Two, Output: 14082
 # Input: Zero point Nine, Output 0.9 
 
+def wordBank(word):
+  switcher = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "ten": 10,
+    "eleven": 11,
+    "twelve": 12,
+    "thirteen": 13,
+    "fourteen": 14,
+    "fifteen": 15,
+    "sixteen": 16,
+    "seventeen": 17,
+    "eighteen": 18,
+    "nineteen": 19,
+    "twenty": 20,
+    "thirty": 30,
+    "forty": 40,
+    "fifty": 50,
+    "sixty": 60,
+    "seventy": 70,
+    "eighty": 80,
+    "ninety": 90,
+    "hundred": 100,
+    "thousand": 1000,
+  }
+  return switcher.get(word)
 
 
 def wordToNumber(word):
-  number = word.split()
-
+  word = word.split()
+  number = map(wordBank, word)
+  
   return number
 
-
-print(wordToNumber('five'))
+print(wordToNumber('three'))
+print(wordToNumber('nineteen'))
+print(wordToNumber('five hundred sixty seven'))
